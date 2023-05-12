4fabb4755b736ec717c7474d635297d50221cc3c
yurigarrido <y.garrido.yuri@gmail.com>
2023-05-12 20:21:58 -0300

    add typescript

---
+++ src/App.tsx
@@ -0,0 +1,61 @@
+import { Header } from "./components/Header"
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
+import { Post,PostType } from "./components/Post"
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'http://github.com/yurigarrido.png',
+      name: 'Yuri Garrido',
+      role: 'Front end Developer',
+    },
+    content: [
+      {type: 'paragraph',content:'Fala galera'},
+      {type: 'paragraph',content:'Acabei de subir mais um projeto no meu portifólio'},
+      {type: 'link',content:'jane.desing/doctorcare'},
+    ],
+    publishedAt: new Date('2023-05-03 20:30:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/LuisCarlosCruz.png',
+      name: 'Luis Carlos',
+      role: 'Desenvolvedor FullStack '
+    },
+    content: [
+      {type: 'paragraph',content:'Fala galera'},
+      {type: 'paragraph',content:'Acabei de subir mais um projeto no meu portifólio'},
+      {type: 'link',content:'jane.desing/doctorcare'},
+    ],
+    publishedAt: new Date('2023-05-01 20:30:00')
+  },
+]
+
+function App() {
+
+  return (
+   <div>
+    <Header/>
+     
+     <div className={styles.wrapper}>
+      <Sidebar/>
+      <main>
+        {posts.map(post  => {
+          return (
+            <Post
+              key={post.id}
+              post={post}
+            />
+          )
+        })}
+      </main>
+     </div>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,22 @@
+import { Avatar } from './Avatar'
+import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react' 
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img className={styles.cover} src="https://images.unsplash.com/photo-1491002052546-bf38f186af56?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" />
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/yurigarrido.png" />
+        <strong>Yuri Garrido</strong>
+        <span>Front-End Dev</span>
+      </div>
+      <footer>
+        <a href="#">
+          <PencilLine size={20}/>
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  )
+}
