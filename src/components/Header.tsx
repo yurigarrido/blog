32bc7c9c05dec2d1bd2495501134b87ac52a6ec2
yurigarrido <y.garrido.yuri@gmail.com>
2023-06-08 00:10:45 -0300

    fix: add data-cy on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import IgniteLogo from '../assets/ignite-logo.svg'
 
 export function Header() {
   return (
-    <header data-testid="header" className={styles.header}>
-      <img src={IgniteLogo} alt="logotipo do ignite" />
+    <header data-testid="header" data-cy="header" className={styles.header}>
+      <img src={IgniteLogo} alt="logotipo do ignite" data-cy="headerImg" />
     </header>
   )
 }
